8bd799fd873f0192dbc98b507ac2efb2947ba0e0
Daniel Hilton <danhilton27@hotmail.com>
2019-09-13 19:11:47 +0100

    Add Search Bar Delegate methods for NewEntryVC.

--- Eat Me/Controller/NewEntryViewController.swift
+++ Eat Me/Controller/NewEntryViewController.swift
@@ -56,6 +56,8 @@ class NewEntryViewController: UITableViewController, UITextFieldDelegate {
         
         tableView.keyboardDismissMode = .interactive
         
+        addInputAccessoryForTextFields(textFields: [foodNameTextField,servingSizeTextField,servingTextField,caloriesTextField,proteinTextField,carbsTextField,fatTextField], dismissable: true, previousNextable: true)
+        
         
     }
     
@@ -181,4 +183,45 @@ class NewEntryViewController: UITableViewController, UITextFieldDelegate {
         
     }
     
+}
+
+
+//Extension to add a toolbar to the top of each textfield
+extension UIViewController {
+    func addInputAccessoryForTextFields(textFields: [UITextField], dismissable: Bool = true, previousNextable: Bool = false) {
+        for (index, textField) in textFields.enumerated() {
+            let toolbar: UIToolbar = UIToolbar()
+            toolbar.sizeToFit()
+            
+            var items = [UIBarButtonItem]()
+            if previousNextable {
+                let previousButton = UIBarButtonItem(image: UIImage(named: "UpArrow"), style: .plain, target: nil, action: nil)
+                previousButton.width = 20
+                if textField == textFields.first {
+                    previousButton.isEnabled = false
+                } else {
+                    previousButton.target = textFields[index - 1]
+                    previousButton.action = #selector(UITextField.becomeFirstResponder)
+                }
+                
+                let nextButton = UIBarButtonItem(image: UIImage(named: "DownArrow"), style: .plain, target: nil, action: nil)
+                nextButton.width = 20
+                if textField == textFields.last {
+                    nextButton.isEnabled = false
+                } else {
+                    nextButton.target = textFields[index + 1]
+                    nextButton.action = #selector(UITextField.becomeFirstResponder)
+                }
+                items.append(contentsOf: [previousButton, nextButton])
+            }
+            
+            let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+            let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: view, action: #selector(UIView.endEditing))
+            items.append(contentsOf: [spacer, doneButton])
+            
+            
+            toolbar.setItems(items, animated: false)
+            textField.inputAccessoryView = toolbar
+        }
+    }
 }
