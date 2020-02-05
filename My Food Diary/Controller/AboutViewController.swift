81b2cd1a67fa2477f913ebec441cca485fdef302
Daniel Hilton <danhilton27@hotmail.com>
2020-02-05 20:08:58 +0000

    Remove 'AnyCodable' framework. Add 'AboutViewController' and link this with 'About' cell on MoreVC. Add image 'AppLogoBlueBackground'. Change LogOut button background colour to sky blue and text colour to white.

---
+++ My Food Diary/Controller/AboutViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AboutViewController.swift
+//  My Food Diary
+//
+//  Created by Daniel Hilton on 05/02/2020.
+//  Copyright © 2020 Daniel Hilton. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UITableViewController {
+
+    @IBOutlet weak var appLogoImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var versionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.tintColor = .white
+        
+        appLogoImageView.layer.cornerRadius = 25
+        titleLabel.textColor = Color.skyBlue
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        versionLabel.text = "Version: \(appVersion ?? "0.0.0")"
+        
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
+    }
+
+
+
+}
--- My Food Diary/Controller/BarcodeScannerViewController.swift
+++ My Food Diary/Controller/BarcodeScannerViewController.swift
@@ -204,7 +204,7 @@ class BarcodeScannerViewController: UIViewController, AVCaptureMetadataOutputObj
                 }
             }
         } else {
-            displayErrorAlert(message: "There was an error retrieving information for this barcode. Please try again or enter details manually.")
+            displayErrorAlert(message: "Information for this barcode could not be found. Please try again or enter details manually.")
             session.stopRunning()
             
         }
@@ -288,7 +288,7 @@ class BarcodeScannerViewController: UIViewController, AVCaptureMetadataOutputObj
                     print("Error parsing JSON - \(error)")
                     
                     self.activityIndicator.stopAnimating()
-                    self.displayErrorAlert(message: "There was an error retrieving information for this barcode. Please try again or enter details manually.")
+                    self.displayErrorAlert(message: "Information for this barcode could not be found. Please try again or enter details manually.")
                 }
                 
             }
